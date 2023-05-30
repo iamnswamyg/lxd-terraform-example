199832bbc36600e317f11f0b03e2bacf62114122
Narayan Swamy Ghanta <iamnswamy@outlook.com>
2023-05-30 16:53:28 +0200

    organised

--- var.tf
+++ var.tf
@@ -7,16 +7,8 @@ variable "salt-network" {
     volume_name    = string
     ipv4   = string
   })
-  default = {
-      profile_name   = "salt-profile"
-      network_name   = "salt-network"
-      pool_name      = "salt-pool"
-      volume_name    = "salt-volume"
-      ipv4   = "192.168.0.1/24"   
-  }
+ 
 }
-
-
 variable "salt-instances" {
   description = "A map of salt configurations"
     type           = map(object({
@@ -25,30 +17,5 @@ variable "salt-instances" {
     ip             = string
     master         = bool
   }))
-  default = {
-    instance1 = {
-      name           = "salt-master"
-      image          = "salt-master"
-      ip             = "192.168.0.2"
-      master         = true
-    }
-    instance2 = {
-      name           = "salt-minion"
-      image          = "salt-minion"
-      ip             = "192.168.0.3"
-      master         = false
-    }
-  }
-}
-
-variable "profile_salt_share" {
-  description = "Name of the LXD profile"
-  type        = string
-  default     = "salt-share"
-}
-
-variable "profile_pillar_share" {
-  description = "Name of the LXD profile"
-  type        = string
-  default     = "pillar-share"
+  
 }
