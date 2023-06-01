891c2b0d52ef6168723bef78ffeead8c65885aab
Narayan Swamy Ghanta <iamnswamy@outlook.com>
2023-06-01 14:33:19 +0200

    thats all!

---
+++ variables.tf
@@ -0,0 +1,82 @@
+variable "profiles" {
+  description = "A map of salt configurations"
+  type = map(object({
+    pool_devices = list(object({
+      name       = string
+      properties = object({
+        pool           = string
+        path           = string
+      })
+    }))
+    share_devices = list(object({
+      name       = string
+      properties = object({
+        source           = string
+        path           = string
+      })
+    }))
+  }))
+}
+variable "pools" {
+ description = "A map of lxd storages"
+    type           = map(object({
+    driver         = string
+  }))
+  
+}
+
+variable "storages" {
+ description = "A map of lxd storages"
+    type           = map(object({
+    pools         = string
+  }))
+  
+} 
+variable "networks" {
+  description = "A map of salt configurations"
+    type           = map(object({
+    ipv4           = string
+    ipv4_enabled   = bool
+    ipv6           = string
+    ipv6_enabled   = bool
+  }))
+  
+}
+
+variable "instances" {
+  description = "A map of salt configurations"
+    type           = map(object({
+    name           = string
+    ephemeral      = bool
+    image          = string
+    profile        = string
+    start          = bool
+    pool_devices = list(object({
+      volume       = string
+      properties = object({
+        pool           = string
+        path           = string
+      })
+    }))
+    share_devices = list(object({
+      name       = string
+      properties = object({
+        source           = string
+        path           = string
+      })
+    }))
+    nic_devices = list(object({
+      name       = string
+      properties = object({
+        network        = string
+        ipv4           = string
+      })
+    }))
+  }))
+    
+  
+}
+
+
+
+
